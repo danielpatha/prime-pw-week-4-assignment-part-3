b75aaaf8f4b613c685362d92cf605cb8627c76e3
Daniel Pathammavong <patha037@umn.edu>
2022-09-11 16:32:14 -0500

    Finished Assignment

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,31 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+//defining the basket array
+let basket = [];
+//addItem function
+function addItem(item, basket){
+    basket.push('shoes');
+   console.log(true); return true;
+}
+addItem('shoes',basket);
+
+console.log(basket);
+
+//listItems function
+function listItems(){
+  for (let i = 0; i < basket.length; i++){
+    console.log(basket[i]);
+  }
+}
+listItems();
+
+//empty function
+function empty(){
+basket.length = 0;
+}
+empty();
+console.log(basket);
+
+
